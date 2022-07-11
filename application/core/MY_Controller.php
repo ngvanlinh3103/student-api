914e199c0a0b9a1f610dcd1e1951f8a40811c7fa
font-End <ngvanlinh31>
2022-07-12 03:17:49 +0700

    update

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,20 +37,61 @@
             header('Content-Type: application/json');
         }
         
+        //get post data 
         public function get_posting_data(){
             $posting_data = $this->input->post();
-
+            // format data oject to strings
             if(NULL === $posting_data || empty($posting_data)){
                 $raw_data = file_get_contents('php://input');
                 if(NULL === $raw_data || empty($raw_data)) return $posting_data;
                 $posting_data = json_decode($raw_data, TRUE);
             }
-            
-            if(!empty($posting_data)){
-                
-            }
-            $posting_data
+
+            return $posting_data;
         }
 
+        public function set_format($value = NULL){ 
+            $this->format = ($value !== NULL) ? $value : "";
+
+            return $this;
+        }
+
+        public function get_format(){
+            return $this->format;
+        }
+
+        public function show_404(){ return $this->failed("404")->render_json();}
+
+        public function render_json($data = NULL) {
+
+            $this->consolidate_data_json($data);
+
+        $this->$data['api_version'] = API_VERSION;
+        }
+
+        private function consolidate_data_json($data = NULL){
+            NULL != $data && $this->array_merge($this->$data, $data);
+
+            $this->date['errors'] = $this->get_error();
+            $this->date['messages'] = $this->get_messages();
+            if(define('SHOW_LOG_DETAILS') && SHOW_LOG_DETAILS){
+                $this->date["server"] = $_SERVER;
+                $this->date["request"] = $_REQUEST;
+                $this->date["post"] = $_POST;
+            }
+
+            return $this;
+        }
+
+        public function get_error(){
+
+            return $this->errors;
+        }
+
+        public function get_messages(){
+
+            return $this->messages;
+        }
+    
     }
 ?>
